a69f97fb2fbb4a4fefce65f17ec46532b6258d0d
miroc_whyixd_desktop <miroc@whyicxd.com>
2025-05-21 12:42:39 +0800

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base:'/ai-document-namer',
+      base:'/ai_document_namer',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
